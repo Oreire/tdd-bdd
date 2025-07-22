229ce6ac2b536b2559e93183fdad8b8091d6454f
LAREDO LANCER <lancerajayi@yahoo.com>
2025-07-22 01:21:11 +0100

    Testing with RSpec

--- RubyTest/test_triangle.rb
+++ RubyTest/test_triangle.rb
@@ -12,17 +12,35 @@ class TestTriangleArea < Test::Unit::TestCase
   end
 
   def test_negative_values
-    assert_equal(-6.0, triangle_area(-3, 4))
-    assert_equal(-6.0, triangle_area(3, -4))
-    assert_equal(6.0, triangle_area(-3, -4))
+    assert_raise_with_message(ArgumentError, 'Base must be a non-negative number') { triangle_area(-3, 4) }
+    assert_raise_with_message(ArgumentError, 'Height must be a non-negative number') { triangle_area(3, -4) }
+    assert_raise_with_message(ArgumentError, 'Base must be a non-negative number') { triangle_area(-3, -4) }
   end
 
   def test_invalid_types
+    # Strings
     assert_raise(TypeError) { triangle_area('3', 4) }
     assert_raise(TypeError) { triangle_area(3, '4') }
     assert_raise(TypeError) { triangle_area('3', '4') }
+
+    # Booleans
     assert_raise(TypeError) { triangle_area(true, 5) }
     assert_raise(TypeError) { triangle_area(5, false) }
-    assert_raise(TypeError) { triangle_area(true, false) }
+
+    # Nil
+    assert_raise(TypeError) { triangle_area(nil, 5) }
+    assert_raise(TypeError) { triangle_area(5, nil) }
+
+    # Arrays and hashes
+    assert_raise(TypeError) { triangle_area([3], 4) }
+    assert_raise(TypeError) { triangle_area(3, {height: 4}) }
+  end
+
+  private
+
+  # Custom helper for error message testing
+  def assert_raise_with_message(exception, message)
+    error = assert_raise(exception) { yield }
+    assert_equal(message, error.message)
   end
 end
--- RubyTest/triangle.rb
+++ RubyTest/triangle.rb
@@ -2,7 +2,16 @@
 
 def triangle_area(base, height)
   unless base.is_a?(Numeric) && height.is_a?(Numeric)
-    raise TypeError, 'Base and height must be numbers'
+    raise TypeError, 'Both base and height must be numbers (Integer or Float)'
   end
+
+  if base < 0
+    raise ArgumentError, 'Base must be a non-negative number'
+  end
+
+  if height < 0
+    raise ArgumentError, 'Height must be a non-negative number'
+  end
+
   0.5 * base * height
 end
